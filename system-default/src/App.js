3a203b4c9e25cb66b0a03533a09270ee1df38be4
MrWatchman <joaooli17@hotmail.com>
2023-04-04 08:04:00 -0300

    Inicio do projeto / login

---
+++ system-default/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import LoginPages from './pages/LoginPages';
+import HomePages from './pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPages />
+    </div>
+  );
+}
+
+export default App;
