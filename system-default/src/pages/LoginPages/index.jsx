d9855372c885bcc07e85524bdf4c10100be8ca9a
JoaoOliveira-Dev <joaooli17@hotmail.com>
2023-04-04 21:57:32 -0300

    style tela de login

--- system-default/src/pages/LoginPages/index.jsx
+++ system-default/src/pages/LoginPages/index.jsx
@@ -1,9 +1,22 @@
+// Imports do REACT
 import React from "react";
+
+// Import do CSS
+import "./style.css";
 
 const LoginPages = () => {
   return (
-    <div>
-      <h1 className="title">Login</h1>
+    <div className="container">
+      <div className="loginBox">
+        <h1 className="title">Login</h1>
+        <form className="form">
+          <input type="text" placeholder="Usuário" />
+          <input type="password" placeholder="Senha" />
+          <button type="submit">Entrar</button>
+        </form>
+        <a href="#">Esqueceu a senha?</a>
+        <a href="#">Criar uma conta</a>
+      </div>
     </div>
   )
 }
